c8c610d2e3746b08b7794245e70668f5bdeb17b5
rrobek <florianfischer@gmx.de>
2016-04-12 21:21:38 +0200

    Corrected paths

--- bin/JapanMapTranslate.bat
+++ bin/JapanMapTranslate.bat
@@ -1,8 +1,6 @@
 @echo off
 rem #! /bin/bash
 set HERE=%~dp0
-set CP=%HERE%;%HERE%kakasi.jar;%HERE%commons-lang-2.6.jar;%HERE%kuromoji-core-0.9.0.jar;%HERE%kuromoji-ipadic-0.9.0.jar
-set KDEF1=-Dkakasi.home=%HERE%
-set KDEF2=-Dkakasi.itaijiDictionary.path=%HERE%dict\itaijidict
+set CP=%HERE%;%HERE%lib\commons-lang-2.6.jar;%HERE%lib\kuromoji-core-0.9.0.jar;%HERE%lib\kuromoji-ipadic-0.9.0.jar
 java -cp %CP% %KDEF1% %KDEF2%  JapanMapTranslate %*
 
